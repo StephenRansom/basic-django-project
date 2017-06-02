1cb8887dda1a325e2c0ea304ca371bcbb209f56e
stephen.ransom <stephen.ransom@ruralsourcing.com>
2017-06-02 15:30:22 -0600

    basic blog functionality, url pattern matching, test posts, authors, tags,categories

--- three/blog/views.py
+++ three/blog/views.py
@@ -13,3 +13,26 @@ def index(request):
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', { 'post':post})
+
+def posts_by_category(request, category_slug):
+    category = Category.objects.get(slug=category_slug)
+    posts = Post.objects.filter(category__slug=category_slug)
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    print(category)
+    return render(request, 'blog/post_by_category.html', context)
+
+def posts_by_tag(request, tag_slug):
+    tag = Tag.objects.get(slug=tag_slug)
+    posts = Post.objects.filter(tags__name=tag)
+    context = {
+        'tag': tag,
+        'posts': posts
+    }
+    return render(request, 'blog/post_by_tag.html', context)

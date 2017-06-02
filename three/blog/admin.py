7efada0bd42238db6b67e9e4a28d3170b129cf5d
stephen.ransom <stephen.ransom@ruralsourcing.com>
2017-06-02 15:56:55 -0600

    added models to admin panel

--- three/blog/admin.py
+++ three/blog/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from blog import models
 # Register your models here.
+
+admin.site.register(models.Post)
+admin.site.register(models.Category)
+admin.site.register(models.Author)
+admin.site.register(models.Tag)

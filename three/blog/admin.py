8ff68dea90075f513bc6407b99816b59ae98ffba
stephen.ransom <stephen.ransom@ruralsourcing.com>
2017-06-05 11:41:48 -0600

    implemented bootstrap, improved admin view models

--- three/blog/admin.py
+++ three/blog/admin.py
@@ -5,7 +5,29 @@ from django.contrib import admin
 from blog import models
 # Register your models here.
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_on')
+    search_fields = ['name', 'email']
+    ordering = ['-name']
+    list_filter = ['active']
+    date_hierarchy = 'created_on'
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date','author','category')
+    search_fields = [ 'title','content']
+    ordering = ['-pub_date']
+    list_filter = ['pub_date']
+    date_hierarchy = 'pub_date'
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name')
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','slug')
+    search_fields = ('name')
+
 admin.site.register(models.Post)
 admin.site.register(models.Category)
-admin.site.register(models.Author)
+admin.site.register(models.Author, AuthorAdmin)
 admin.site.register(models.Tag)
--- three/blog/models.py
+++ three/blog/models.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
 # Create your models here.
 class Author(models.Model):
+    """post authors"""
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     active = models.BooleanField(default=False)
